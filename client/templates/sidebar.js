59bd89c3c7b46ff8eeb257085836295a04c182e5
Andre Amorim - Note <andre.php6@gmail.com>
2016-12-03 15:10:04 -0200

    avatar

--- client/templates/sidebar.js
+++ client/templates/sidebar.js
@@ -1,10 +1,9 @@
 import { Template } from 'meteor/templating';
-import { Meteor } from 'meteor/meteor';
+//import { Meteor } from 'meteor/meteor';
 
 
 Template.sidebar.helpers({
-	usuario(){
-		var nameUser = Meteor.user().profile.name;
-		return nameUser;
-	}
+	userPicHelper: function() {
+		
+    }
 });
