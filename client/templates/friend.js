06132de124ab985746a25647c6f1a6be1f0020d5
Andre Amorim <andre.php6@gmail.com>
2016-12-07 22:27:07 -0200

    list emails in friends

--- client/templates/friend.js
+++ client/templates/friend.js
@@ -1,12 +1,19 @@
 import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
 
+Meteor.subscribe('friends.listEmails');
+
 Template.friends.rendered = function(){
     $("#amigos").select2({
     	placeholder: "digite o email do seu amigo",
     });
-
-    Meteor.call('users.list', {}, function (e, result) {
-    	console.log(result);
-	});
 };
+
+Template.friends.helpers({
+	allUsers() { 
+		return Meteor.users.find({}); 
+	},
+  	email(){
+  		return this.emails[0].address;
+  	}
+});
--- server/api/friend.js
+++ server/api/friend.js
@@ -2,8 +2,12 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 Meteor.methods({
-	'users.list'(){
-		console.log(Meteor.users.find());
-		return Meteor.users.find();
+	'users.list'() {
+		return "A6";
 	}
+});
+
+Meteor.publish('friends.listEmails',function(){
+  // you should restrict this publication to only be available to admin users
+  return Meteor.users.find({},{fields: { emails: 1 }});
 });
