08a6c6c525fded106579caea8e837580dc2a9069
andre <andre.php6@gmail.com>
2016-12-15 13:34:48 -0200

    fixed bug style in avatar

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import { Template } from 'meteor/templating';
 T9n.setLanguage('pt_BR');
 
 Avatar.setOptions({
-  defaultImageUrl: "https://www.guidesmiths.com/uploads/images/1450466520567_avatar-default_1.png"
+  defaultImageUrl: "https://www.guidesmiths.com/uploads/images/1450466520567_avatar-default_1.png",
+  cssClassPrefix: "custon-avatar"
 });
 
